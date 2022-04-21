da1096fe838d56eae8ff17717fc572469e7ed372
Thu Juli <thujuli037@gmail.com>
2022-04-22 03:30:42 +0800

    bug upload image from request user

---
+++ Portofolio Website/frontend/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import Project
+
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
--- Portofolio Website/frontend/views.py
+++ Portofolio Website/frontend/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Project
+from .forms import ProjectForm
 
 
 def home(request):
-    return render(request, 'frontend/index.html')
+    listProject = Project.objects.all()
+    context = {
+        'listProject': listProject}
+    return render(request, 'frontend/index.html', context)
+
+
+def createPoject(request):
+    form = ProjectForm()
+
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'frontend/create-project.html', context)
