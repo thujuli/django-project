80b6cd59ab679fa584f306f0018f54e07f99d7ae
Thu Juli <thujuli037@gmail.com>
2022-05-09 19:01:52 +0800

    mvt ArticleCategoryListView

--- Simple Blog/blogs/urls.py
+++ Simple Blog/blogs/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ArticleListView, ArticleDetailView
+from .views import ArticleListView, ArticleDetailView, ArticleCategoryListView
 
 app_name = 'blogs'
 
 urlpatterns = [
+    path('<str:category>/<int:page>/',
+         ArticleCategoryListView.as_view(), name='category'),
     path('detail/<slug:slug>/', ArticleDetailView.as_view(), name='detail'),
-    path('<str:page>/', ArticleListView.as_view(), name='list'),
+    path('<int:page>/', ArticleListView.as_view(), name='list'),
 ]
--- Simple Blog/blogs/views.py
+++ Simple Blog/blogs/views.py
@@ -4,12 +4,39 @@ from .models import Article
 # Create your views here.
 
 
+class ArticleCategoryListView(ListView):
+    model = Article
+    template_name = 'blogs/article-category_list.html'
+    context_object_name = 'article_list'
+    paginate_by = 3
+    ordering = ['-published']
+
+    def get_queryset(self, *args, **kwargs):
+        self.queryset = self.model.objects.filter(
+            category=self.kwargs['category'])
+        return super().get_queryset(*args, **kwargs)
+
+    def get_context_data(self, *args, **kwargs):
+        category_list = self.model.objects.values_list(
+            'category', flat=True).distinct().exclude(category=self.kwargs['category'])
+        self.kwargs.update({'category_list': category_list})
+        kwargs = self.kwargs
+        return super().get_context_data(*args, **kwargs)
+
+
 class ArticleListView(ListView):
     model = Article
     template_name = 'blogs/article_list.html'
     context_object_name = 'article_list'
     paginate_by = 3
     ordering = ['-published']
+
+    def get_context_data(self, *args, **kwargs):
+        category_list = self.model.objects.values_list(
+            'category', flat=True).distinct()
+        self.kwargs.update({'category_list': category_list})
+        kwargs = self.kwargs
+        return super().get_context_data(*args, **kwargs)
 
 
 class ArticleDetailView(DetailView):
