98730126bb0e08e0c4f62ff8d6407f546848cc23
Thu Juli <thujuli037@gmail.com>
2022-05-17 19:09:03 +0800

    initial commit

---
+++ social_book/core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    profile_image = models.ImageField(
+        upload_to='profile_images/', default='default.jpg')
+    bio = models.TextField(blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
