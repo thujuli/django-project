4f213ed9b7170eb461ac3bb992787f6508423c25
Thu Juli <thujuli037@gmail.com>
2022-05-18 14:05:34 +0800

    adding feature logout

--- social_book/core/urls.py
+++ social_book/core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.signin, name='login'),
+    path('logout/', views.logoutView, name='logout'),
 ]
--- social_book/core/views.py
+++ social_book/core/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 from .forms import CustomUserCreationForm
 
 from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
+
+
+@login_required(login_url='login')
+def logoutView(request):
+    logout(request)
+    return redirect('login')
 
 
 def signin(request):
@@ -18,7 +25,7 @@ def signin(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('signup')
+            return redirect('signup')  # change to settings url
         else:
             messages.info(
                 request, 'You have entered an invalid username or password')
@@ -61,7 +68,7 @@ def signup(request):
                 else:
                     messages.info(
                         request, 'Something went wrong please try again')
-                    return redirect('login')  # change to login url
+                    return redirect('login')
 
         else:
             messages.info(request, 'Password Not Match')
